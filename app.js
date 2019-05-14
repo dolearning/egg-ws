c450cb7c6d2ffb895c9a8a3710cbd59944bbafea
Cong Liu <cong.liux@gmail.com>
2019-05-14 17:43:50 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ws = require('./lib/ws');
+
+module.exports = app => {
+  ws(app);
+};
---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-ws default config
+ * @member Config#ws
+ * @property {String} SOME_KEY - some description
+ */
+exports.ws = {
+
+};
